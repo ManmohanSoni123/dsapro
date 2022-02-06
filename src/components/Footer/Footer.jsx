e8148d0e646a262c83f4aedb0710a1eb7e2b6611
Niteen-K <nkgnk6@gmail.com>
2022-02-06 15:35:59 +0530

    added footer file

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <Typography variant='h6' >©Copyright to Manmohan Soni Brajesh Kumar Niteen Kumar</Typography>
+    </footer>
+  );
+}
+export default Footer;
